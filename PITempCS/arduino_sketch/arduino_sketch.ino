4aab2e36d37e5f407f1c8566622be290bb12818f
stop-pattern <railwaysandstationfan+Git@gmail.com>
2021-03-27 23:02:10 +0900

    [change] 基板テスト用
    受け取る部分がintなせいで1[km/h]ごとの表示しかできない気がするが平気なのか？

--- PITempCS/arduino_sketch/arduino_sketch.ino
+++ PITempCS/arduino_sketch/arduino_sketch.ino
@@ -1,38 +1,32 @@
-char input[10];   // 文字列格納用
-int i = 0;  // 文字数のカウンタ
-String reads;
-int speed = 0;
-int am = 0;
+// for SpeedometerDriveBoard rev.2
+// this source si modded by stop-pattern@github
+// original: saha209@github
+// settings is below:
+// board: ESP32 Dev Module
 
-#define INTERP(xi,xi1,yi,yi1,x) (yi + ((( yi1 - yi ) * ( x - xi )) / ( xi1 - xi )))
-
-const int ar1[] = {
-  0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140,
-  0, 2, 4, 6, 9, 12, 15, 18, 21, 25, 28, 32, 35, 39, 42, 46, 49, 53, 56, 60, 63, 67, 70, 73, 76, 79, 82, 85, 89,
-};
-
-const int ar2[] = {
-  0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140,
-  0, 2, 4, 6, 9, 12, 15, 18, 21, 25, 28, 32, 35, 39, 42, 46, 49, 53, 56, 60, 63, 67, 70, 73, 76, 79, 82, 85, 89,
-};
+// setting
+const int SPD = 19;
+const int ledcChannel = 0;
+const int ledcBaseFreq = 1000;
+const int ledcTimerBit = 16;
 
 void setup() {
-  pinMode(PB4, OUTPUT);
-  pinMode(PB10, OUTPUT);
+  // serial setup
   Serial.begin(19200);
+  while(!Serial) delay(1);
+  Serial.println("Serial connected!");
+
+  // pwm setup
+  pinMode(SPD, OUTPUT);
+  ledcSetup(ledcChannel, ledcBaseFreq, ledcTimerBit);
+  ledcAttachPin(SPD, ledcChannel);
 }
 
-int interp1dim(const int x, const int*ar) {
-  int q;
-  if (x <= ar[0]) {
-    return ar[29];
-  } else if (x >= ar[29 - 1]) {
-    return ar[29 * 2 - 1];
-  }
-  for (q = 1; q < 29; q++) {
-    if (ar[q] >= x) break;
-  }
-  return INTERP(ar[q - 1], ar[q], ar[q + 29 - 1], ar[q + 29], x);
+// 0-120の範囲で速度を受け取ってpwmで出力
+void pwmWrite(int speed = 0) {
+  speed = abs(speed);
+  if(speed > 120) speed = 120;
+  ledcWrite(0, pow(2, ledcTimerBit) * speed / 120);
 }
 
 void loop() {
@@ -41,27 +35,14 @@ void loop() {
   Serial.print("ab\n");
   if (Serial.available()) {
     //速度計情報を受信
-    reads = Serial.readStringUntil('\n');
+    String reads = Serial.readStringUntil('\n');
     if (reads.substring(0, 2) == "ab") {
       reads = reads.substring(2);
-      speed = reads.toInt(); 
-      analogWrite(PB10, interp1dim(speed, ar1));
+      int speed = reads.toInt(); 
+      pwmWrite(speed);
     }
   }
-
-  //電流計情報を要求
-  Serial.print("ai\n");
-  if (Serial.available()) {
-    //電流計情報を受信
-    reads = Serial.readStringUntil('\n');
-    if (reads.substring(0, 2) == "ai") {
-      reads = reads.substring(2);
-      am = reads.toInt(); 
-      analogWrite(PB4, interp1dim(am, ar1));
-    }
-  }
-
-  
+  delay(100);
 }
 
 //ソースは第一閉塞進行様のホームページより改変
